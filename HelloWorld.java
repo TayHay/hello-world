8826973f9119b452178ab1062e2bd202aebe29cf
Tayhay <tbhaynes91@gmail.com>
2021-02-03 23:17:41 -0500

    Revert "Ran to get class.  Changed "Hellow" to "Hello""
    This reverts commit 83d12e1f731f8bd9872a7a28232abf219348a378.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
    public static void main (String[] args) {
-      System.out.println("Hello world!");
-      System.out.println("Program modified, but able to revert " + "to original version using GitHub Desktop");
+      System.out.print("Hellow world!");
    } //end of main
 } //end of program
