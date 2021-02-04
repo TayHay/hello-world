83d12e1f731f8bd9872a7a28232abf219348a378
Tayhay <tbhaynes91@gmail.com>
2021-02-03 23:15:51 -0500

    Ran to get class.  Changed "Hellow" to "Hello"
    Ran to get class.  Changed "Hellow" to "Hello".

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
    public static void main (String[] args) {
-      System.out.print("Hellow world!");
+      System.out.println("Hello world!");
+      System.out.println("Program modified, but able to revert " + "to original version using GitHub Desktop");
    } //end of main
 } //end of program
